e6404043c0a426030d2b0cc39b6b2266a7fdee45
Piero <pierocarneiro2908@gmail.com>
2023-11-24 19:15:28 -0500

    added generics

--- API/Program.cs
+++ API/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<StoreContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 
 var app = builder.Build();
 
