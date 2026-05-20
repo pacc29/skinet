08f76f9bcee3a529bc4ec04833e47bc63b4f4d35
Piero Carneiro <pierocarneiro2908@gmail.com>
2026-05-20 13:00:02 -0500

    Added navbar

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBar } from "./nav-bar/nav-bar";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('client');
+  protected readonly title = signal('skinet');
 }
---
+++ client/src/app/nav-bar/nav-bar.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons"
+
+@Component({
+  selector: 'app-nav-bar',
+  imports: [FontAwesomeModule],
+  templateUrl: './nav-bar.html',
+  styleUrl: './nav-bar.css',
+})
+export class NavBar {
+  faShoppingCart = faShoppingCart
+}
